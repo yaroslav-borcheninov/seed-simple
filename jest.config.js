62af548382b39f0df6d50149cd89b8c35fdcd117
Yaroslav Borcheninov <yaroslav-borcheninov@users.noreply.github.com>
2019-04-20 23:46:35 +0100

    Remove enzyme

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,5 @@ module.exports = {
   clearMocks: true,
   collectCoverage: true,
   collectCoverageFrom: ["src/**/*.{ts,tsx}"],
-  setupFiles: ["./enzyme.config.js"],
   setupFilesAfterEnv: ["react-testing-library/cleanup-after-each"],
-  snapshotSerializers: ["enzyme-to-json/serializer"],
 }
